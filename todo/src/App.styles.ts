623f181e49bf35215c377cf4c5de2a127c16d1e6
Romulo Costa <romulocoliveirag@gmail.com>
2022-01-10 23:43:32 -0300

    commit inicial

---
+++ todo/src/App.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    background-color: #17181F;
+    color: #797A81;
+    min-height: 100vh;
+
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+
+`;
+
+export const Header = styled.h1`
+    padding: 0;
+    margin: 0;
+    color: #FFF;
+    text-align: center;
+    border-bottom: 2px solid #fff;
+    padding-bottom: 20px;
+
+`;
---
+++ todo/src/components/AddArea/index.tsx
@@ -0,0 +1,32 @@
+import { useState, KeyboardEvent } from 'react';
+import * as C from './styles';
+
+
+type Props = {
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({ onEnter }: Props) => {
+    const [inputText, setInputText] = useState('');
+    console.log(inputText)
+    
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if (e.code === 'Enter' && inputText !== '') {
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
+
+    return (
+        <C.Container>
+            <div className="image">➕</div>
+            <input
+                type="text"
+                placeholder="Adcionar tarefa"
+                value={inputText}
+                onChange={e => setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+            />
+        </C.Container>
+    );
+}
